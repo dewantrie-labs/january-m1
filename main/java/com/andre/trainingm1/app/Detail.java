ffd3e9af53871d7cdf64b511af5eb773e9fbe90c
andre hardian faisal <andre@neo-fusion.com>
2014-12-30 17:31:48 +0700

    application M1 update

---
+++ main/java/com/andre/trainingm1/app/Detail.java
@@ -0,0 +1,19 @@
+package com.andre.trainingm1.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Detail extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+
+
+}
