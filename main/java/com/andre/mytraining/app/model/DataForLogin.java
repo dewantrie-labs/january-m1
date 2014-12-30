efd0724ad8e7f3bcb9815ec0024bc6eb512ed1f3
andre hardian faisal <andre@neo-fusion.com>
2014-12-30 17:17:52 +0700

    application M1

---
+++ main/java/com/andre/mytraining/app/model/DataForLogin.java
@@ -0,0 +1,11 @@
+package com.andre.mytraining.app.model;
+
+/**
+ * Created by Andree on 12/22/2014.
+ */
+public class DataForLogin {
+    private String name;
+    private String errorMessage;
+    private Boolean success;
+
+}
